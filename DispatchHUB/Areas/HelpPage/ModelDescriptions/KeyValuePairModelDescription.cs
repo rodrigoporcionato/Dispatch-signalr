019a6c36f86d11a74dfc94e15bc703ea8883cfb5
Rodrigo Porcionato Allves <rodrigoporcionato@gmail.com>
2020-02-02 14:24:40 -0300

    Revert "Revert "ajustes""
    This reverts commit 28e715a180ff6ae3d4f155e64e1905c9de3c27d1.

--- DispatchHUB/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace DispatchHUB.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
