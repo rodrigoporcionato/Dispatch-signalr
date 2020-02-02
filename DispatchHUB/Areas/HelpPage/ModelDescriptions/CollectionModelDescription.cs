7d760c8935effeb55ada81ee9e7994f5516cff86
Rodrigo Porcionato Allves <rodrigoporcionato@gmail.com>
2020-02-02 14:24:52 -0300

    Revert "ajustes"
    This reverts commit fb6aef889559280a038fe78a5e6280f60e5cfa4d.

---
+++ DispatchHUB/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DispatchHUB.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
